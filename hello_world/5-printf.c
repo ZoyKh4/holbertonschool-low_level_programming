ab72d39870f30850c809de5743da0108dcd9d32b
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-18 13:35:44 +0000

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main (- I want that which surpasses even the merciful liberation from death. Thorfinn)
+*Return: I have no ennemies.
+*/
+
+int main(void)
+
+{
+
+printf("with proper grammar, but the outcome is a piece of art,");
+return(0);
+}

6db570a9a7b4cc385b7dff12b9ca69c43e7b502b
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-18 18:34:27 +0000

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main (- I want that which surpasses even the merciful liberation from death.)
+*Return: I have no ennemies.
+*/
+
+int main(void)
+{
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+Printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+return (0);
+}

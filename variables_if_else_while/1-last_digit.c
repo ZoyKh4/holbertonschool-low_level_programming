78cc0dc5926730add48bca283accec464bb48440
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-20 16:03:48 +0200

    1-last_digit.c

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - thorfinn
+ * description 'trust the process'
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	int n;
+	int pipi;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	pipi = n % 10;
+	if (pipi > 5)
+{
+	printf("%d and is greater than 5\n", n);
+}
+	else if (pipi < 6 && pipi != 0)
+{
+	printf("%d and is less than 6 and not 0\n", n);
+}
+	else if (pipi == 0)
+	{
+		printf("%d and is 0\n", n);
+	}
+	return (0);
+}

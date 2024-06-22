aa170e7eea4995359fc8ebb8ee3fe113a12856f6
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-22 14:12:07 +0200

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - thorfinn
+ * description "trust the process"
+ */
+
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+	putchar(letter);
+}
+putchar('\n');
+	return (0);
+}

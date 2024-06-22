9e01657c25de35c420c472dc896ec8812a7c9583
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-22 15:10:48 +0200

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - write numbers
+ *
+ * Description: trust the process
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+char num;
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}

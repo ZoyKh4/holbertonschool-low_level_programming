490f9bef581289506d1dfd1a7ed26da5f53f67f4
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-22 15:51:45 +0200

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,14 +10,14 @@ int main(void)
 char num;
 for (num = '0'; num <= '9'; num++)
 {
-	putchar(num);
-	if (num < '9')
-	{
-		putchar(',');
-		putchar(' ');
-		continue;
-	}
+putchar(num);
+if (num < '9')
+{
+putchar(',');
+putchar(' ');
+continue;
+}
 }
 putchar('\n');
-	return (0);
+return (0);
 }

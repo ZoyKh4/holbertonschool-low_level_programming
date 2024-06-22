def8752f10983b2dfcf3d53c88eb4bb4566233c9
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-22 15:40:35 +0200

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - thorfinn
+ * Description: trust the process
+ * Return always 0
+ */
+
+int main(void)
+
+{
+char letter;
+char num;
+
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num);
+}
+
+for (letter = 'a'; letter <+ 'f; letter++')
+{
+putchar(letter);
+}
+putchar("\n");
+return (0);
+}

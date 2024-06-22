85c1cd28446bc1516e151be0787d2f947d694052
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-22 14:37:54 +0200

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - etry point of th program
+ *
+ * descripton: trustthe process 
+ * 
+ * return: always 0
+*/
+
+int main(void)
+{
+char letter;
+char Uletter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+	putchar(letter);
+}
+for (Uletter = 'A'; Uletter <= 'Z'; Uletter++)
+{
+	putchar(Uletter);
+}
+putchar('\n');
+	return (0);
+}

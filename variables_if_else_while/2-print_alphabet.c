3178af56db12ad2a9c1cd91e0cc9c14ca672b055
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-22 14:24:54 +0200

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Desc de la fonction
- * descrition: main
- * return: always 0
+ * main - Entry point of the program
+ *
+ * Description: Prints the alphabet 
+ *
+ * Return: Always 0 (Success).
  */
-
 int main(void)
 {
-char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-{
-	putchar(letter);
-}
-putchar('\n');
-	return (0);
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        putchar(letter);
+    }
+    putchar('\n');
+    return (0);
 }

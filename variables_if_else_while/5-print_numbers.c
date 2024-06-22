da751e763b9f0d9388409239c9c35abaab9e5645
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-22 15:02:36 +0200

    5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * 
+ *
  * main - write numbers
- * 
+ *
  * Return: always 0
  */
 
 int main(void)
 {
 char num;
-for (num = '0'; num <='9'; num++)
+for (num = '0'; num <= '9'; num++)
 {
 putchar(num);
 }

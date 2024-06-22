b1df405f46762763d92d8b16f7d28ee25609841a
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-22 15:41:59 +0200

    8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -17,10 +17,10 @@ for (num = '0'; num <= '9'; num++)
 putchar(num);
 }
 
-for (letter = 'a'; letter <+ 'f; letter++')
+for (letter = 'a'; letter <+ 'f'; letter++')
 {
 putchar(letter);
 }
-putchar("\n");
+putchar('\n');
 return (0);
 }

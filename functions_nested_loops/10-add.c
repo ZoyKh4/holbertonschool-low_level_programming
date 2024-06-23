65ed60f9b08f51d1edcda594670369fd50815331
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-23 22:18:59 +0200

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - no ennemies.
+ * @n: be in peace
+ * @c: there is nothing to lose
+ *
+ *
+ * Return: le resultat
+ */
+
+int add(int n, int c)
+{
+return (c + n);
+}

2b033edb6f909bb38b5f9facadba695723380338
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-23 14:08:51 +0200

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int main(void) 
 {
-write(1, "_putchar\n", 9); // code to write _putchar
-return 0;
+write(1, "_putchar\n", 9); /* code to write _putchar */
+return (0);
 }

967982e7133a0efddf13e42b85d49b8a8f8de1cf
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-23 16:54:49 +0200

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha - lettre ou non 
+ * @c: caractere a check
+ * Return - 1 ou 2
+ */
+
+int _isalpha(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

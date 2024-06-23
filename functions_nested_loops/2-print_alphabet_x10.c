5ebceb1bd993a980e83556d0b6c1e88c0a9d0017
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-23 16:25:56 +0200

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ * print-alphabet_x10 - fonction main write alph 10 time 
+ */
+
+void print_alphabet_x10(void)
+{
+    char alphabet;
+for (time = '0'; time < '10'; time++)
+{
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+_putchar('n\');
+}
+}

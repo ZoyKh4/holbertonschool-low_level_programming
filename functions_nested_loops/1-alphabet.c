0a125fde170b006a04f532e33d821e53bde37e5c
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-23 14:51:26 +0200

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - ecrire l alphabet comme un grand
+ */
+
+void print_alphabet(void)
+
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++);
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}

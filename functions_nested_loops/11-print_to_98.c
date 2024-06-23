df41c9819c748b3025d399297d56081c8a3bfa04
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-23 22:21:10 +0200

    11-print_to_98.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - try ur best
+ * @n: be in peace
+ *
+ * Return: trust ur selfs
+ */
+
+void print_to_98(int n)
+{
+for (; n > 98; n--)
+{
+printf("%d, ", n);
+}
+for (; n < 98; n++)
+{
+printf("%d, ", n);
+}
+printf("%d\n", n);
+}

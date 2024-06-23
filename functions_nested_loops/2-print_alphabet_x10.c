02acc0e11e28ca69f8db1eab18858d47161da1ac
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-23 16:30:51 +0200

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,13 @@
 void print_alphabet_x10(void)
 {
     char alphabet;
+    int time;
 for (time = '0'; time < '10'; time++)
 {
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 _putchar(alphabet);
 }
-_putchar('n\');
+_putchar('\n');
 }
 }

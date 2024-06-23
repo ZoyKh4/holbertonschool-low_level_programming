9ec916594294020d21b28538d7d86a294c0a8384
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-23 14:10:18 +0200

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,11 @@
 #include <unistd.h>
 #include <stdio.h>
+/**
+ * main - c le main quoi
+ * Return: always 0
+ */
 
-int main(void) 
+int main(void)
 {
 write(1, "_putchar\n", 9); /* code to write _putchar */
 return (0);

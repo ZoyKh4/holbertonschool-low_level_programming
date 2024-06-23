095ded7c36b343d2629daa5237cca52419fb31f9
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-23 17:20:58 +0200

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Main function
+ *
+ * Return : minutes
+ */
+
+void jack_bauer(void)
+{
+int h = 0;
+int m = 0;
+
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
+{
+_putchar(h / 10 + '0');
+_putchar(h % 10 + '0');
+_putchar(':');
+_putchar(m / 10 + '0');
+_putchar(m % 10 + '0');
+_putchar('\n');
+}
+}
+}

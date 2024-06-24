2e124378a84d1d2d588b9d8f8f40873c79f2442d
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-24 14:01:36 +0200

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ *@a: print numbers
+ * Description: no ennemies
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+char a;
+for (a = '0'; a <= '9')
+{
+_putchar(a);
+}
+_putchar("\n");
+}

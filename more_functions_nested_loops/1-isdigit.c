de8821fef597e9b8ee7810643b4f49afa8ccc3c9
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-24 12:13:07 +0200

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - permet decheck
+ * @c:caractere a check
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+
+return (1);
+
+else
+
+return (0);
+
+}

9941547f7c70005acbaba7d5d1e645f8291b0543
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-24 16:08:15 +0200

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers - print numbers
+ * print_most_numbers - print numbers
  *: print numbers
  * Description: no ennemies
  * Return: 0
@@ -9,12 +9,15 @@
 void print_most_numbers(void)
 {
 int n;
-{
+
 for (n = '0'; n <= '9'; n++)
-if (n != 2 && n != 4)
+{
+
+if (n == '2' || n == '4')
+continue;
 {
 _putchar(n);
 }
-_putchar('\n');
 }
+_putchar('\n');
 }

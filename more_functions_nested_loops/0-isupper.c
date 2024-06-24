0d407a5436f40c7397f9b903f0bb8b510ac9c4d5
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-24 11:57:17 +0200

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - permet decheck
+ * @c:caractere a check
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

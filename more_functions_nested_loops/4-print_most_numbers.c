a47be57b521de06f5b039f9cd15d01d4f1870096
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-24 15:48:03 +0200

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ *: print numbers
+ * Description: no ennemies
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+int n;
+{
+for (n = '0'; n <= '9'; n++)
+if (n != 2 && n != 4)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
+}

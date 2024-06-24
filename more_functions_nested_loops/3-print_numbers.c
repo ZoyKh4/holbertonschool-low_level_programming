6203ac2577e78ac4558a539550abb8085274f302
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-24 14:44:38 +0200

    3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
  * print_numbers - print numbers
- *@a: print numbers
+ *: print numbers
  * Description: no ennemies
  * Return: 0
  */
 
 void print_numbers(void)
 {
-char a;
-for (a = '0'; a <= '9')
+int n;
 {
-_putchar(a);
+for (n = '0'; n <= '9'; n++)
+{
+_putchar(n);
 }
-_putchar("\n");
-return;
+_putchar('\n');
+}
 }

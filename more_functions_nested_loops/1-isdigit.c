116cd6ce5711157003e8bb3604b5bd9c9163e6d0
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-24 12:55:24 +0200

    1-isdigit.c

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _isdigit - isUpper savoir si c'est une maj.
- * @c: variable
- * Return: Always 0 pour une miniscule. et 1 pour MAJ
+ * _isdigit - permet decheck
+ * @c:caractere a check
+ * Return: 1 or 0
  */
 
 int _isdigit(int c)
-
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (0);
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }

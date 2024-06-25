cc1924cadede759abf71838bc6c91779a4c6943f
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-25 14:16:49 +0200

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * 
+ * 
+ * 
+ */
+
+void more_numbers(void)
+
+{
+int n;
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+for (n = 0; n <= 14; n++)
+{
+if (n > 9)
+{
+_putchar(n / 10 + '0');
+}
+_putchar(n % 10 + '0');
+}
+_putchar('\n');
+}
+}

30810a7c8e93ed0e6d95df9e1b72299deb5338d9
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-27 14:55:49 +0200

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * 
+ * 
+ */
+
+int main(void)
+
+{
+int a
+
+for (a = 1; a <= 100; a++)
+{
+if (a % 15 == 0)
+{
+printf("FizzBuzz")
+}
+else
+if (a % 3 == 0)
+{
+printf("Fizz")
+}
+else
+if (a % 5 == 0)
+{
+printf("Buzz")
+}
+printf("\n")
+}
+}

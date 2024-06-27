2882cca386deef6bc8ffbfab0e3e51432c5f6c07
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-27 15:06:47 +0200

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
 /**
- * 
- * 
+ * main - prototype
+ *@a: variable
  */
 
 int main(void)
 
 {
-int a
+int a;
 
 for (a = 1; a <= 100; a++)
 {
 if (a % 15 == 0)
 {
-printf("FizzBuzz")
+printf("FizzBuzz");
 }
 else
 if (a % 3 == 0)
 {
-printf("Fizz")
+printf("Fizz");
 }
 else
 if (a % 5 == 0)
 {
-printf("Buzz")
+printf("Buzz");
 }
-printf("\n")
+else
+{
+printf("%d", a);
+}
+if (a < 100)
+{
+printf(" ");
+}
+printf("\n");
 }
 }

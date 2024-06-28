d69d11cce60fc85f5c45ce82885a477f08873dfa
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-29 00:26:15 +0200

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - variable
+ *@s: pointeur
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+int a;
+
+for (a = 0; s[a] !='\0'; a++)
+
+{
+
+}
+return (a);
+}

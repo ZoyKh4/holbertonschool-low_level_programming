4fef94a5030495a4ef2d2a898f034ee559f8270b
ZoyKh4 <kerimgmc45@gmail.com>
2024-06-30 17:59:53 +0200

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - print inverser
+ *@s: char s
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+int longueur = strlen(s);
+int i;
+for (i = longueur - 1; i >= 0; i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
+}
